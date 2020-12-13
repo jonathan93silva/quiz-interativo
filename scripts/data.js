3e603d6a5521dd1499a807543ac2471774157c5a
jonathan93silva <jonathan93silva@gmail.com>
2020-12-13 18:14:09 -0300

    update versao 0.2

---
+++ scripts/data.js
@@ -0,0 +1,78 @@
+
+const questionsAndAnswers = [
+    {
+        question: '"Brasil" é um pais pertencente ao continente',
+        alternatives: ['Africano', 'Americano'],
+        id_theme: 1,
+        correctAnswer: 'B'
+    },
+    {
+        question: '"Egito" é um pais pertencente ao(s) continente(s)',
+        alternatives: ['Africano/Asiático', 'Europeu/Asiático'],
+        id_theme: 1,
+        correctAnswer: 'A'
+    },
+    {
+        question: '"Azerbaijão" é um pais pertencente ao(s) continente(s)',
+        alternatives: ['Europeu/Asiático', 'Africano/Asiático'],
+        id_theme: 1,
+        correctAnswer: 'A'
+    },
+    {
+        question: '"Mongólia" é um pais pertencente ao continente',
+        alternatives: ['Oceânico', 'Asiático'],
+        id_theme: 1,
+        correctAnswer: 'B'
+    },
+    {
+        question: '"Mongólia" é um pais pertencente ao continente',
+        alternatives: ['Africano', 'Americano'],
+        id_theme: 1,
+        correctAnswer: 'B'
+    },
+    {
+        question: '"Mongólia" é um pais pertencente ao continente',
+        alternatives: ['Africano', 'Americano'],
+        id_theme: 2,
+        correctAnswer: 'B'
+    },
+    {
+        question: '"Mongólia" é um pais pertencente ao continente',
+        alternatives: ['Africano', 'Americano'],
+        id_theme: 2,
+        correctAnswer: 'B'
+    },
+    ,
+    {
+        question: '"Mongólia" é um pais pertencente ao continente',
+        alternatives: ['Africano', 'Americano'],
+        id_theme: 3,
+        correctAnswer: 'B'
+    },
+    {
+        question: '"Mongólia" é um pais pertencente ao continente',
+        alternatives: ['Africano', 'Americano'],
+        id_theme: 3,
+        correctAnswer: 'B'
+    },
+    ,
+    {
+        question: '"Mongólia" é um pais pertencente ao continente',
+        alternatives: ['Africano', 'Americano'],
+        id_theme: 3,
+        correctAnswer: 'B'
+    },
+    {
+        question: '"Mongólia" é um pais pertencente ao continente',
+        alternatives: ['Africano', 'Americano'],
+        id_theme: 4,
+        correctAnswer: 'B'
+    }
+]
+
+const themes = [
+    {id:1, description:"Geografia"},
+    {id:2, description:"História"},
+    {id:3, description:"Fisica"},
+    {id:4, description:"Conhecimentos gerais"}
+]
---
+++ scripts/script.js
@@ -0,0 +1,106 @@
+
+const form = document.querySelector('.form-alternatives')
+const popup = document.querySelector('.popup-wrapper')
+const selectThemes = document.querySelector('.select-themes')
+const result = document.querySelector('.result')
+
+let themeIssues = null
+
+function renderThemes(){
+
+    const insertOptionsInSelect = ({ id, description }) => {
+        const option = document.createElement('option')
+
+        option.value = id
+        option.textContent = description
+        
+        selectThemes.append(option)
+    }
+
+    selectThemes.textContent = ''
+    themes.forEach(insertOptionsInSelect)
+}
+
+function renderQuestionsAndAnswers(){
+    themeIssues = questionsAndAnswers
+        .filter(({ id_theme }) => id_theme == selectThemes.value)
+    
+    const insertQuestionsInForm = ({ question, alternatives }, index) => {
+
+        const containerQuestion = document.createElement('div')
+
+        containerQuestion.className = "mb-4"
+        containerQuestion.innerHTML = `
+                <p class="lead font-weight-normal">${index+1}. ${question}:</p>
+                <div class="ml-4">
+                    <label>
+                        <input type="radio" name="inputQuestion${index+1}" value="A" checked>
+                        "${alternatives[0]}"
+                    </label>
+                </div>
+                <div class="ml-4">
+                    <label>
+                        <input type="radio" name="inputQuestion${index+1}" value="B">
+                        "${alternatives[1]}"
+                    </label>
+                </div>
+        `
+        
+        form.append(containerQuestion)
+        closeResult()
+    }
+
+    form.innerHTML = ''
+    themeIssues.forEach(insertQuestionsInForm)
+
+    const button = document.createElement('button')
+    
+    button.classList.add('btn', 'btn-success', 'btn-block')
+    button.textContent = 'Enviar'
+
+    form.append(button)
+}
+
+function closeResult(){
+    result.classList.add('d-none')
+}
+
+function showResult(event){
+    event.preventDefault()
+    
+    const timeAnimation = 10
+    
+    let score = 0
+    let counter = 0
+
+    const computePoints = ({ correctAnswer }, index) => {
+        const isCorrectResponse = form[`inputQuestion${index+1}`].value == correctAnswer
+        const pointsForQuestions = (100 / themeIssues.length)
+        
+        if(isCorrectResponse) score += pointsForQuestions
+    }
+
+    const countAnimation = () => {
+        const totalPoints = counter === score
+        
+        if(totalPoints) clearInterval(timer)
+
+        result.querySelector('span').textContent = `${counter}%`
+        counter++
+    }
+
+    themeIssues.forEach(computePoints)
+    result.classList.remove('d-none')
+    result.style.opacity = 1
+    scrollTo(0,0)
+    
+    const timer = setInterval(countAnimation, timeAnimation)
+}
+
+window.addEventListener('load', () => {
+    renderThemes()
+    renderQuestionsAndAnswers()
+})
+form.addEventListener('submit', showResult)
+selectThemes.addEventListener('change', renderQuestionsAndAnswers)
+result.querySelector('button').addEventListener('click', closeResult)
